cb6fb5a60e646ac9264519f0c053d07036ea14df
cristimc8 <cristimc8@gmail.com>
2020-11-20 13:47:13 +0200

    xx

--- App/Blueprints/Dashboard/gameRoom.py
+++ App/Blueprints/Dashboard/gameRoom.py
@@ -3,7 +3,7 @@ from flask import render_template, url_for
 from .base import gameRoom
 
 
-@gameRoom.route('/room/<code>')
-def profile(code):
-    return render_template('gameRoom.html', title = 'Game Room', code = code)
+@gameRoom.route('/room')
+def handle_index():
+    return render_template('gameRoom.html', title = 'Game Room')
     
